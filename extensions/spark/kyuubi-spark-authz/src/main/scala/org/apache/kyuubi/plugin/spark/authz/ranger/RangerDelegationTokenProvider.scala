e874e964a31e32c5b5ba55d7fed5cc3b8ce4243c
Andrei Shitov <sai@arenadata.io>
2026-02-18 18:33:10 +0300

    feat(ranger): added delegation token support.

---
+++ extensions/spark/kyuubi-spark-authz/src/main/scala/org/apache/kyuubi/plugin/spark/authz/ranger/RangerDelegationTokenProvider.scala
@@ -0,0 +1,98 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.kyuubi.plugin.spark.authz.ranger
+
+import scala.util.control.NonFatal
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.security.{Credentials, SecurityUtil, UserGroupInformation}
+import org.apache.ranger.admin.client.RangerAdminRESTClient
+import org.apache.spark.SparkConf
+import org.apache.spark.security.HadoopDelegationTokenProvider
+import org.slf4j.LoggerFactory
+
+class RangerDelegationTokenProvider extends HadoopDelegationTokenProvider {
+
+  private val LOG = LoggerFactory.getLogger(getClass)
+
+  private val PROPERTY_PREFIX = "ranger.plugin.spark"
+
+  private lazy val adminClient = {
+    val conf = SparkRangerAdminPlugin.getRangerConf
+    val serviceName = conf.get(PROPERTY_PREFIX + ".service.name", "spark")
+    val client = new RangerAdminRESTClient()
+    client.init(serviceName, "sparkSubmit", PROPERTY_PREFIX, conf)
+    client
+  }
+
+  override def serviceName: String = "ranger"
+
+  override def delegationTokensRequired(
+      sparkConf: SparkConf,
+      hadoopConf: Configuration): Boolean = {
+    UserGroupInformation.isSecurityEnabled
+  }
+
+  override def obtainDelegationTokens(
+      hadoopConf: Configuration,
+      sparkConf: SparkConf,
+      creds: Credentials): Option[Long] = {
+    try {
+      val rangerUrl = SparkRangerAdminPlugin.getRangerConf
+        .get(PROPERTY_PREFIX + ".policy.rest.url")
+
+      if (rangerUrl == null || rangerUrl.isEmpty) {
+        LOG.warn("Cannot obtain Ranger delegation token: " +
+          PROPERTY_PREFIX + ".policy.rest.url is not configured")
+        return None
+      }
+
+      val renewer = getTokenRenewer(sparkConf, hadoopConf)
+
+      LOG.info(s"Obtaining Ranger delegation token from $rangerUrl, renewer=$renewer")
+
+      val token = adminClient.getDelegationToken(renewer)
+
+      creds.addToken(token.getService, token)
+
+      val ident = token.decodeIdentifier()
+      LOG.info(s"Obtained Ranger delegation token, " +
+        s"owner=${ident.getUser}, renewer=$renewer, maxDate=${ident.getMaxDate}")
+
+      None
+    } catch {
+      case NonFatal(e) =>
+        LOG.warn(s"Failed to obtain Ranger delegation token. " +
+          s"If Ranger is not used, set spark.security.credentials.ranger.enabled=false", e)
+        None
+    }
+  }
+
+  private[ranger] def getTokenRenewer(
+      sparkConf: SparkConf,
+      hadoopConf: Configuration): String = {
+    val master = sparkConf.get("spark.master", "")
+    if (master.contains("yarn")) {
+      val rmPrincipal = hadoopConf.get("yarn.resourcemanager.principal")
+      if (rmPrincipal != null && rmPrincipal.nonEmpty) {
+        return SecurityUtil.getServerPrincipal(rmPrincipal, null: String)
+      }
+    }
+    UserGroupInformation.getCurrentUser.getUserName
+  }
+}
---
+++ extensions/spark/kyuubi-spark-authz/src/test/scala/org/apache/kyuubi/plugin/spark/authz/ranger/RangerDelegationTokenProviderSuite.scala
@@ -0,0 +1,112 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.kyuubi.plugin.spark.authz.ranger
+
+import java.net.InetAddress
+import java.util.ServiceLoader
+
+import scala.collection.JavaConverters._
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.security.{Credentials, UserGroupInformation}
+import org.apache.spark.SparkConf
+import org.apache.spark.security.HadoopDelegationTokenProvider
+// scalastyle:off
+import org.scalatest.funsuite.AnyFunSuite
+
+class RangerDelegationTokenProviderSuite extends AnyFunSuite {
+// scalastyle:on
+
+  private val provider = new RangerDelegationTokenProvider()
+
+  test("serviceName is ranger") {
+    assert(provider.serviceName === "ranger")
+  }
+
+  test("ServiceLoader discovers RangerDelegationTokenProvider") {
+    val providers = ServiceLoader.load(classOf[HadoopDelegationTokenProvider])
+      .iterator().asScala.toSeq
+    val rangerProvider = providers.find(_.serviceName == "ranger")
+    assert(rangerProvider.isDefined)
+    assert(rangerProvider.get.isInstanceOf[RangerDelegationTokenProvider])
+  }
+
+  test("delegationTokensRequired returns false when security is disabled") {
+    // Default test env has simple authentication (no Kerberos)
+    assert(!provider.delegationTokensRequired(new SparkConf(), new Configuration()))
+  }
+
+  test("getTokenRenewer - YARN with RM principal") {
+    val sparkConf = new SparkConf().set("spark.master", "yarn")
+    val hadoopConf = new Configuration(false)
+    hadoopConf.set("yarn.resourcemanager.principal", "yarn/rm-host@REALM")
+
+    val renewer = provider.getTokenRenewer(sparkConf, hadoopConf)
+    assert(renewer === "yarn/rm-host@REALM")
+  }
+
+  test("getTokenRenewer - YARN with _HOST placeholder resolves to local hostname") {
+    val sparkConf = new SparkConf().set("spark.master", "yarn")
+    val hadoopConf = new Configuration(false)
+    hadoopConf.set("yarn.resourcemanager.principal", "yarn/_HOST@REALM")
+
+    val renewer = provider.getTokenRenewer(sparkConf, hadoopConf)
+    val expectedHost = InetAddress.getLocalHost.getCanonicalHostName.toLowerCase
+    assert(renewer === s"yarn/$expectedHost@REALM")
+  }
+
+  test("getTokenRenewer - YARN without RM principal falls back to current user") {
+    val sparkConf = new SparkConf().set("spark.master", "yarn")
+    val hadoopConf = new Configuration(false)
+
+    val renewer = provider.getTokenRenewer(sparkConf, hadoopConf)
+    assert(renewer === UserGroupInformation.getCurrentUser.getUserName)
+  }
+
+  test("getTokenRenewer - non-YARN uses current user") {
+    val sparkConf = new SparkConf().set("spark.master", "local[2]")
+    val hadoopConf = new Configuration(false)
+    hadoopConf.set("yarn.resourcemanager.principal", "yarn/rm-host@REALM")
+
+    val renewer = provider.getTokenRenewer(sparkConf, hadoopConf)
+    // RM principal is ignored for non-YARN master
+    assert(renewer === UserGroupInformation.getCurrentUser.getUserName)
+  }
+
+  test("getTokenRenewer - no spark.master defaults to current user") {
+    val sparkConf = new SparkConf()
+    val hadoopConf = new Configuration(false)
+
+    val renewer = provider.getTokenRenewer(sparkConf, hadoopConf)
+    assert(renewer === UserGroupInformation.getCurrentUser.getUserName)
+  }
+
+  test("obtainDelegationTokens returns None when Ranger URL is not configured") {
+    // Use a fresh Configuration without ranger-spark-security.xml properties
+    val hadoopConf = new Configuration(false)
+    val sparkConf = new SparkConf()
+    val creds = new Credentials()
+
+    // SparkRangerAdminPlugin.getRangerConf has the test config with a dummy URL,
+    // but if we could override it, this would test the null URL path.
+    // For now, test that obtainDelegationTokens handles REST client failure gracefully.
+    val result = provider.obtainDelegationTokens(hadoopConf, sparkConf, creds)
+    assert(result.isEmpty)
+    assert(creds.getAllTokens.isEmpty)
+  }
+}
