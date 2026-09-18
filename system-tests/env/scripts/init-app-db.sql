97a0535fb33d30d1a6e59e42e3bf26ce1fb41c60
valeriy <v.aistov@arenadata.io>
2026-09-18 11:02:20 +0300

    Revert "Added system-tests for integration tests with full environment, spark(3,4 version), trino, hive, postgresql"
    This reverts commit 456e7aed475d434fcca12be17211a0858a89dff9.

--- system-tests/env/scripts/init-app-db.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS smoke_probe (
-  id INT PRIMARY KEY,
-  label TEXT NOT NULL
-);
-INSERT INTO smoke_probe (id, label) VALUES (1, 'kyuubi-system-tests')
-  ON CONFLICT (id) DO NOTHING;
